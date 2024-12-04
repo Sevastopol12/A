73924066e840a97fa404af150641e8dc54ac01be
Sevastopol <141613215+Sevastopol12@users.noreply.github.com>
2024-12-04 19:28:40 +0700

    Add files via upload

---
+++ Boat/data/Employee.sql
@@ -0,0 +1,204 @@
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(1,'Terri Vorley','141-838-6314',TO_DATE('1996-09-19', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(2,'Humfrey Thorbon','105-602-4238',TO_DATE('1996-04-15', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(3,'Aleen Wilbud','238-699-1801',TO_DATE('1998-01-11', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(4,'Ahmad Trassler','410-466-8436',TO_DATE('1995-05-02', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(5,'Ofella Brignall','670-254-1458',TO_DATE('2000-02-23', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(6,'Faye McGiven','587-987-5021',TO_DATE('1996-09-06', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(7,'Dukey Collacombe','103-898-4179',TO_DATE('2000-09-04', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(8,'Gris Spurryer','627-241-5294',TO_DATE('1998-09-04', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(9,'Keefe Lindley','197-564-5808',TO_DATE('1997-12-03', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(10,'Agathe Greenshields','254-738-8121',TO_DATE('1998-10-28', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(11,'Ivan Goldsmith','801-505-1789',TO_DATE('2000-06-06', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(12,'Davie Maffioni','421-937-3466',TO_DATE('1996-12-19', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(13,'Linea Pound','758-139-8736',TO_DATE('2000-08-31', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(14,'Kristien Pudding','792-120-0765',TO_DATE('1996-09-12', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(15,'Kass Marshman','963-906-2897',TO_DATE('1999-03-19', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(16,'Tim Gaisford','470-672-2766',TO_DATE('1998-03-11', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(17,'Jackelyn Katzmann','691-685-1271',TO_DATE('1995-10-25', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(18,'Bendicty Stading','908-510-6982',TO_DATE('1995-11-22', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(19,'Norean Spratt','258-175-3748',TO_DATE('2000-06-07', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(20,'Pedro Comelli','932-223-0148',TO_DATE('1995-10-26', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(21,'Gard Abrahamson','651-607-2159',TO_DATE('1996-05-31', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(22,'Georgy Zanussii','890-472-5446',TO_DATE('1997-06-23', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(23,'Grata Byk','121-744-1528',TO_DATE('1997-02-04', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(24,'Booth Worvill','538-227-8225',TO_DATE('2000-12-16', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(25,'Fee Mayo','783-714-6122',TO_DATE('1998-01-31', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(26,'Marlow Haston','438-447-4324',TO_DATE('1999-03-11', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(27,'Odelle Olney','196-766-8707',TO_DATE('1997-01-24', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(28,'Davina Derill','288-713-9636',TO_DATE('1996-02-14', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(29,'Lynelle Hardwicke','930-109-8281',TO_DATE('1997-06-14', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(30,'Kylie Morefield','206-173-5233',TO_DATE('1995-04-10', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(31,'Rebekah Dutson','278-102-6659',TO_DATE('2000-08-18', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(32,'Michail Traves','908-890-9320',TO_DATE('2000-12-11', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(33,'Demetra Pinnegar','917-264-7775',TO_DATE('1999-11-14', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(34,'Ki Brimblecomb','579-276-2125',TO_DATE('1995-04-22', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(35,'Gillian Etches','989-260-7347',TO_DATE('1999-08-18', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(36,'Kleon Dun','852-340-5325',TO_DATE('1995-04-28', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(37,'Rahal Punter','785-820-4801',TO_DATE('1999-05-17', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(38,'Milt Gandar','685-699-3695',TO_DATE('1999-07-02', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(39,'Kyle Gervaise','774-444-8361',TO_DATE('1996-05-03', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(40,'Shandra Cauldwell','393-815-2817',TO_DATE('1996-06-29', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(41,'Levon Denness','779-130-6787',TO_DATE('1995-12-08', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(42,'Jinny Volleth','517-586-1869',TO_DATE('1995-03-12', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(43,'Yvonne Welland','793-219-7567',TO_DATE('1995-03-06', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(44,'Jayne Blees','118-331-9960',TO_DATE('1999-01-29', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(45,'Tessi Willicott','571-908-4201',TO_DATE('2000-07-05', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(46,'Bastien Leban','142-570-8963',TO_DATE('2000-07-17', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(47,'Mar Di Maggio','337-345-2965',TO_DATE('1997-07-05', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(48,'Fernanda Marley','490-834-7520',TO_DATE('1999-04-13', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(49,'Malina Fetteplace','685-522-4284',TO_DATE('2000-08-14', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(50,'Mikel Guest','945-184-4685',TO_DATE('1997-04-28', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(51,'Miriam Presslie','555-645-9241',TO_DATE('1995-11-19', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(52,'Beale Cordelle','223-735-7584',TO_DATE('2000-02-13', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(53,'Jerrylee Shackell','611-942-4782',TO_DATE('1998-07-21', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(54,'Brigit De Bruyne','775-486-5987',TO_DATE('1995-03-30', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(55,'Donnajean Graine','374-296-8305',TO_DATE('1995-08-21', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(56,'Lizzy Drewell','951-688-5179',TO_DATE('2000-05-17', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(57,'Alikee Brisard','636-376-1174',TO_DATE('1995-11-06', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(58,'Caralie Reams','682-535-0834',TO_DATE('1999-12-13', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(59,'Webb Gilardoni','753-335-2436',TO_DATE('1999-08-27', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(60,'Jacklyn Nansom','535-408-7215',TO_DATE('1996-05-10', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(61,'Ariel Lacky','178-821-8349',TO_DATE('1995-06-09', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(62,'Debbi Baiss','417-844-8111',TO_DATE('1997-04-24', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(63,'Opaline McCrorie','510-381-2724',TO_DATE('2000-05-30', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(64,'Kitty Aldiss','758-420-8493',TO_DATE('1999-04-01', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(65,'Kaspar Adolphine','394-286-2022',TO_DATE('1995-11-25', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(66,'Ree Gilberthorpe','588-901-1668',TO_DATE('1996-03-24', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(67,'Jacintha Sibbons','601-265-8155',TO_DATE('1995-01-25', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(68,'Farra Kleinsinger','242-832-6840',TO_DATE('1997-05-15', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(69,'Nariko Boarleyson','467-738-9007',TO_DATE('1998-03-15', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(70,'Valma Priver','984-653-6712',TO_DATE('1997-07-23', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(71,'Keeley Caple','313-973-0951',TO_DATE('2000-09-19', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(72,'Christy Spinetti','724-188-8357',TO_DATE('2000-10-06', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(73,'Harp McLardie','722-541-1046',TO_DATE('1996-03-15', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(74,'Lamar Vedmore','639-266-2137',TO_DATE('1998-07-25', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(75,'Dorothee Olner','444-679-5831',TO_DATE('1998-12-11', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(76,'Walsh Lorinez','835-209-7857',TO_DATE('1995-01-30', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(77,'Dill Hurdman','148-119-2231',TO_DATE('1997-07-31', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(78,'Jeri Amorts','121-461-5612',TO_DATE('1999-10-06', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(79,'Debor Reader','352-500-3186',TO_DATE('1997-07-15', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(80,'Fulton Kebbell','244-548-8549',TO_DATE('1998-07-15', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(81,'Wenonah Hantusch','122-699-5522',TO_DATE('1997-08-27', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(82,'Allen Ybarra','292-709-9353',TO_DATE('1995-11-01', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(83,'Joe Ouver','692-405-4187',TO_DATE('1995-07-06', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(84,'Richardo Aleksankov','277-698-1970',TO_DATE('1995-10-02', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(85,'Enoch Nutten','636-896-6535',TO_DATE('2000-08-11', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(86,'Udale Chaffey','373-911-2904',TO_DATE('1998-01-08', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(87,'Vasili Vasyunichev','855-354-4512',TO_DATE('2000-12-28', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(88,'Tomkin Coveney','106-219-3026',TO_DATE('1998-06-07', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(89,'Derward Adrien','898-436-6137',TO_DATE('1998-09-27', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(90,'Cletis Hamon','243-525-2142',TO_DATE('1996-04-22', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(91,'Aubert Mash','600-151-7553',TO_DATE('1999-11-24', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(92,'Hartwell Trehearn','307-606-0238',TO_DATE('1999-01-06', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(93,'Dani Maylott','715-297-5790',TO_DATE('1998-03-22', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(94,'Archibold Girling','876-293-8536',TO_DATE('2000-07-05', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(95,'Sander McGiffie','770-209-2838',TO_DATE('1995-11-21', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(96,'Guillema Agar','800-260-8385',TO_DATE('1997-08-31', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(97,'Fernanda Dartnall','198-130-6890',TO_DATE('1997-09-28', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(98,'Shandie Grutchfield','848-588-2087',TO_DATE('1998-08-14', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(99,'Devonne Hansemann','511-944-0128',TO_DATE('1998-04-08', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(100,'Pier Llop','727-984-7614',TO_DATE('1999-01-03', 'YYYY-MM-DD'));
+INSERT INTO EMPLOYEE (EmployeeID, EmployeeName, Phone, Birthday) VALUES
+(101,'Emelita Stratten','801-680-8336',TO_DATE('1995-06-17', 'YYYY-MM-DD'));
+COMMIT;
+--Employee
---
+++ Boat/main.sql
@@ -0,0 +1,116 @@
+ALTER SESSION SET CURRENT_SCHEMA = mocnhien;
+SET DEFINE OFF;
+COMMIT;
+
+--Employee
+CREATE TABLE EMPLOYEE (
+    EmployeeID INT,
+    EmployeeName VARCHAR2(50) NOT NULL,
+    Phone VARCHAR2(12),
+    Birthday DATE,
+
+    CONSTRAINT pk_emp PRIMARY KEY (EmployeeID)
+);
+
+
+-- BOAT
+CREATE TABLE BOATS (
+    BoatID INT,
+    BoatType VARCHAR2(50) NOT NULL,
+    BoatClass VARCHAR2(100),
+    Make VARCHAR2(50),
+    ProduceDate DATE NOT NULL,
+    DryWeight_LB NUMBER,
+    NumEngines NUMBER,
+    Length_FT NUMBER,
+    Price NUMBER,
+    
+    CONSTRAINT pk_BoatID PRIMARY KEY (BoatID)
+)
+PARTITION BY RANGE (ProduceDate)
+INTERVAL (NUMTODSINTERVAL(365, 'DAY'))
+SUBPARTITION BY LIST (BoatType)
+SUBPARTITION TEMPLATE
+(
+    SUBPARTITION sp_power VALUES ('power'),
+    SUBPARTITION sp_sail VALUES ('sail'),
+    SUBPARTITION sp_unpowered VALUES ('unpowered')
+)
+(
+    PARTITION p_2020 VALUES LESS THAN (TO_DATE('2021-01-01', 'YYYY-MM-DD'))
+);
+
+
+-- Sales
+-- Date range from 2021-01-01 -> 2025-02-01
+CREATE TABLE SALES (
+    SaleID NUMBER,
+    EmployeeID NUMBER NOT NULL,
+    BoatID NUMBER NOT NULL,
+    SaleDate DATE NOT NULL,
+    Quantity NUMBER,
+    TotalAmount NUMBER(10, 2),
+    
+    CONSTRAINT pk_sales PRIMARY KEY (SaleID),
+    CONSTRAINT fk_sales FOREIGN KEY (BoatID) REFERENCES BOATS(BoatID)
+)
+PARTITION BY RANGE(SaleDate) 
+(
+    PARTITION p_2021 VALUES LESS THAN (TO_DATE('2022-01-01', 'YYYY-MM-DD')),
+    PARTITION p_2022 VALUES LESS THAN (TO_DATE('2023-01-01', 'YYYY-MM-DD')),
+    PARTITION p_2023 VALUES LESS THAN (TO_DATE('2024-01-01', 'YYYY-MM-DD')),
+    PARTITION p_CurrentYear VALUES LESS THAN (MAXVALUE)
+);
+
+
+--Boat inventory
+CREATE TABLE INVENTORY (
+    BoatID INT,
+    QuantityInStock INT,
+
+    CONSTRAINT fk_BoatID FOREIGN KEY (BoatID) REFERENCES BOATS(BoatID)
+);
+
+
+--After sales services
+-- Date range from 2022-01-01 -> 2024-12-01
+--ServiceType = ["Maintenance", "EngineRepair", "Customization", "ESR", "Winterization"] 
+--Note: ESR = Electrical System Repair
+CREATE TABLE Services (
+    ServiceDate DATE NOT NULL,                        
+    ServiceType VARCHAR2(100), 
+    ServiceCost NUMBER(10, 2),                        
+    WarrantyCovered VARCHAR2(3) CHECK (WarrantyCovered IN ('YES', 'NO'))   
+)
+PARTITION BY RANGE (ServiceDate) 
+SUBPARTITION BY LIST(ServiceType) 
+SUBPARTITION TEMPLATE
+(
+    SUBPARTITION sp_Maintenance VALUES ('Maintenance'),
+    SUBPARTITION sp_EngineRepair VALUES ('EngineRepair'),
+    SUBPARTITION sp_Customization VALUES ('Customization'),
+    SUBPARTITION sp_ESR VALUES ('ESR'),
+    SUBPARTITION sp_Winterization VALUES ('Winterization')
+)
+(
+    PARTITION p_2021 VALUES LESS THAN (TO_DATE('2022-01-01', 'YYYY-MM-DD')),
+    PARTITION p_2022 VALUES LESS THAN (TO_DATE('2023-01-01', 'YYYY-MM-DD')),
+    PARTITION p_2023 VALUES LESS THAN (TO_DATE('2024-01-01', 'YYYY-MM-DD')),
+    PARTITION p_CurrentYear VALUES LESS THAN (MAXVALUE)
+);
+COMMIT;
+
+
+
+DROP TABLE BOATS;
+DROP TABLE SALES;
+DROP TABLE EMPLOYEE;
+DROP TABLE SERVICES;
+DROP TABLE INVENTORY;
+COMMIT;
+
+
+
+SELECT TABLE_NAME, PARTITION_NAME, subpartition_name, NUM_ROWS 
+FROM USER_TAB_SUBPARTITIONS
+WHERE TABLE_NAME IN ('BOATS', 'SALES', 'SERVICES');
